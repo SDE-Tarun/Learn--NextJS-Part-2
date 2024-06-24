567ceb4c98b2e8eb2e62a3f917dc0a352a6e8657
Tarun <tarunkumar23j@gmail.com>
2024-06-24 11:49:59 +0530

    Main page.js cleaned, Id folder deleted and css for body removed

--- app/page.js
+++ app/page.js
@@ -1,34 +1,8 @@
-'use client'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const page = () => {
-
-  const [users, setusers] = useState([])
-
-  const getUsers = async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users")
-    const data = res.data
-    // console.log(data);
-    setusers(data);
-  }
-
-  useEffect(() => {
-    getUsers() 
-  }, [])
-  
-
   return (
-    <>
-    <button onClick={getUsers} className='bg-green-600 text-white px-4 py-2 rounded font-bold'>Get Data</button>
-    <div className='p-8 bg-slate-100 mt-5'>
-      <ul>
-        {users.map((e)=>{
-          return <li>{e.username} --- <a href={`/${e.id}`}>Explore</a></li>
-        })}
-      </ul>
-    </div>
-    </>
+    <div>page</div>
   )
 }
 
