997cfe68c1d470fe0094545c9335af9a29f80359
Tarun <tarunkumar23j@gmail.com>
2024-06-24 10:32:33 +0530

    Don

---
+++ app/Contact/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Link from 'next/link';
+
+const contact = () => {
+  return (
+    <>
+    <h1 className='text-2xl font-bold'>This is Contact Page</h1>
+    <Link href='/'>Go Back To Home Page</Link>
+    </>
+  )
+}
+
+export default contact
